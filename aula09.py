093def4eba252ae3cd05c6ca4d8eb2277edcaf68
pholyana nyele <pholyanarodrigues6@gmail.com>
2025-04-08 11:23:01 -0300

    aula10

---
+++ aula09.py
@@ -0,0 +1,29 @@
+numero=int(input("Digite um numero"))
+if  numero >=1 and numero <= 12:
+    if numero == 1:
+        print("janeiro")
+    elif numero  == 2:
+        print("fevereiro")
+    elif numero == 3:
+        print("março")
+    elif numero == 4:
+        print("abril")
+    elif numero == 5:
+        print("maio")
+    elif numero == 6:
+        print("junho")
+    elif numero == 7:
+        print("julho")
+    elif numero == 8:
+        print("agosto")
+    elif numero == 9 :
+        print("setembro")
+    elif numero == 10 :
+        print("outubro")
+    elif numero == 11:
+        print("novembro")
+    elif numero == 12 :
+        print("dezembro")
+
+else:
+    print("numero invalido")
